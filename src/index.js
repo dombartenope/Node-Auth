92a0b0e144f3d39d8a946d1a2becf5dd72aa8e41
Dom Bartenope <dominickbartenope@yahoo.com>
2021-09-18 16:59:51 -0400

    Basic HTTP Node server code added, added index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-import "./db.js"
+import { fastify } from 'fastify';
+import fastifyStatic from 'fastify-static';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-console.log('Hello World');
+//ESM specific feature
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = fastify();
+
+async function startApp() {
+    try {
+
+        app.register(fastifyStatic, {
+            root: path.join(__dirname, "public"),
+        })
+        
+        app.get('/', {}, (request, reply) => {
+            reply.send({
+                data: "Hello World",
+            })
+        })
+
+        await app.listen(8000);
+        console.log('Server listening on port 8000')
+
+    } catch (e) {
+
+        console.error(e);
+
+    }
+}
+
+startApp();
